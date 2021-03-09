9907eb75e1f55437d1755fbb089275bffa8005fe
wslmacieiradev <wslmacieira@outlook.com>
2021-03-08 23:15:34 -0300

    Introdução ao TypeScript para Angular

---
+++ hello-typescript/main.ts
@@ -0,0 +1,26 @@
+var minhaVar = 'minha Variavel';
+
+function minhaFunc(x, y) {
+    return x + y;
+}
+
+// ES 6 ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+
+numeros.map(function (valor) {
+    return valor * 2;
+});
+
+numeros.map(valor => valor * 2);
+
+class Matematica {
+    soma(x, y) {
+        return x + y
+    }
+}
+
+var n1 = 'asdasda';
+n1 = 4;
