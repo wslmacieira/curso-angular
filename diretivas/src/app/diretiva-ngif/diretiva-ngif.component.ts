5527ffeb86d2cf74ee9c2f924cd3cb5eb8cc5e67
wslmacieiradev <wslmacieira@outlook.com>
2021-03-19 15:54:58 -0300

    Diretivas ngIf

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgifComponent implements OnInit {
 
+  cursos = [];
+
+  mostrarCursos = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onMostrarCursos(): void {
+    this.mostrarCursos = !this.mostrarCursos;
   }
 
 }
