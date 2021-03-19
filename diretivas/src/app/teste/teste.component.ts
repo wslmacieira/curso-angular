c0ba0a060bcf0204679b2c423dda70309708e405
wslmacieiradev <wslmacieira@outlook.com>
2021-03-19 11:48:23 -0300

    ng lint, ng test, ng e2e

--- diretivas/src/app/teste/teste.component.ts
+++ diretivas/src/app/teste/teste.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-teste',
   templateUrl: './teste.component.html',
-  styleUrls: ['./teste.component.css']
+  styleUrls: ['./teste.component.scss']
 })
 export class TesteComponent implements OnInit {
+
+  minhaVariavel: string;
 
   constructor() { }
 
