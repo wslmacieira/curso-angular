5a0b7276e5ee6f06537ccd1b583b9b441c9a2902
wslmacieiradev <wslmacieira@outlook.com>
2021-03-21 10:48:58 -0300

    Injetando um serviço em outro serviço

--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { LogService } from '../shared/log.service';
 @Injectable()
 export class CursosService {
   static criouNovoCurso = new EventEmitter<string>();
@@ -6,15 +7,17 @@ export class CursosService {
 
   private cursos = ['Angular', 'React', 'Node.js'];
 
-  constructor() {
+  constructor(private logService: LogService) {
     console.log('CursosService');
   }
 
   getCursos(): string[] {
+    this.logService.consoleLog('Obtendo lista de cursos');
     return this.cursos;
   }
 
   addCurso(curso: string): void {
+    this.logService.consoleLog(`Criando um novo curso ${curso}`);
     this.cursos.push(curso);
     this.emitirCursoCriado.emit(curso);
     CursosService.criouNovoCurso.emit(curso);
