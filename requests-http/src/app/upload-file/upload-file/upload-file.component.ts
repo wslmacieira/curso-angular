3d45ede82a009d1f0b3a681fe42c38840004ad68
wslmacieiradev <wslmacieira@outlook.com>
2021-04-22 12:07:18 -0300

    Criando operador RxJS customizado

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,5 +1,6 @@
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs-operators';
 import { environment } from 'src/environments/environment';
 import { UploadFileService } from '../upload-file.service';
 
@@ -35,18 +36,26 @@ export class UploadFileComponent implements OnInit {
   onUpload(): void {
     if (this.files && this.files.size > 0) {
       this.service.upload(this.files, environment.BASE_URL + '/upload')
-        .subscribe((event: HttpEvent<object>) => {
-          // HttpEventType
-          console.log(event.type);
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        )
+        .subscribe(response => console.log('Upload Concluido'));
+      // .subscribe((event: HttpEvent<object>) => {
+      //   // HttpEventType
+      //   console.log(event.type);
 
-          if (event.type === HttpEventType.Response) {
-            console.log('Upload Concluido');
-          } else if (event.type === HttpEventType.UploadProgress) {
-            const percentDone = Math.round((event.loaded * 100) / (event.total ? event.total : 100));
-            this.progress = percentDone;
-            console.log(this.progress);
-          }
-        });
+      //   if (event.type === HttpEventType.Response) {
+      //     console.log('Upload Concluido');
+      //   } else if (event.type === HttpEventType.UploadProgress) {
+      //     const percentDone = Math.round((event.loaded * 100) / (event.total ? event.total : 100));
+      //     this.progress = percentDone;
+      //     // console.log(this.progress);
+      //   }
+      // });
     }
   }
 
