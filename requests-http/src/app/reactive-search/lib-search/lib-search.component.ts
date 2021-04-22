7899713f50e6da6b2fe8396faf5644f3c08c13a0
wslmacieiradev <wslmacieira@outlook.com>
2021-04-22 14:45:12 -0300

    Passando Parâmetros na URL (HttpParams)

--- requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lib-search',
@@ -9,14 +12,37 @@ import { FormControl } from '@angular/forms';
 export class LibSearchComponent implements OnInit {
 
   queryField = new FormControl();
+  readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  results$: Observable<any>;
+  total: number;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   onSearch(): void {
-    console.log(this.queryField.value);
+    const fields = 'name,description,version,homepage';
+    let value = this.queryField.value;
+
+    const params_ = {
+      search: value,
+      fields
+    };
+
+    let params = new HttpParams();
+
+    params = params.set('search', value);
+    params = params.set('fields', fields);
+
+    if (value && value.trim() !== '') {
+      value = value.trim();
+      this.results$ = this.http.get(this.SEARCH_URL, { params })
+        .pipe(
+          tap((res: any) => this.total = res.total),
+          map((res: any) => res.results)
+        );
+    }
   }
 
 }
